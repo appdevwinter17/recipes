9318a13ba7bd227e6fbd0f389a8e078e852eaf87
Raghu Betina <raghu@firstdraft.com>
2017-03-01 23:44:44 +0000

    Validate presence of comments.comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ApplicationRecord
 
   # Validations
 
+  validates :comment, :presence => { :message => "Must be associated to user" }
+
   validates :comment, :length => { :minimum => 0, :maximum => 400 }
 
   validates :user_id, :presence => true
