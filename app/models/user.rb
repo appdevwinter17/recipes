bfacfad407c6901dd2b4850f1fd8fbdab47cb38c
Raghu Betina <raghu@firstdraft.com>
2017-03-01 23:45:30 +0000

    Indirect association between users and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,14 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :followers,
+             :through => :friend_requests_received,
+             :source => :sender
+
+  has_many   :leaders,
+             :through => :friend_requests_sent,
+             :source => :recipient
+
   has_many   :commented_posts,
              :through => :comments,
              :source => :post
